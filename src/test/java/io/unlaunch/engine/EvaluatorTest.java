4e42b4df58e4c46708a531805cbf1851306afe97
Umer Mansoor <umermk3@gmail.com>
2020-12-25 18:04:23 -0800

    Added more unit tests for bucketing (percentage rollout)

--- src/test/java/io/unlaunch/engine/EvaluatorTest.java
+++ src/test/java/io/unlaunch/engine/EvaluatorTest.java
@@ -11,7 +11,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.mockito.Mockito;
@@ -30,7 +32,7 @@ public class EvaluatorTest {
     final String flagKey = "flag123";
     final String varKeyON = "ON";
     final String varKeyOFF = "OFF";
-    final String varKeyDefaultRule = "defaultRule";
+    final String generalVariation = "variation";
     
     Evaluator evaluator = Mockito.mock(Evaluator.class);
     FeatureFlag flag = Mockito.mock(FeatureFlag.class);
@@ -38,83 +40,7 @@ public class EvaluatorTest {
 
     @Before
     public void callBeforeEachTest() {
-        evaluator = Mockito.mock(Evaluator.class);
-
-        flag = Mockito.mock(FeatureFlag.class);
-        when(flag.isEnabled()).thenReturn(true);
-        when(flag.getKey()).thenReturn(flagKey);
-
-        List<Rule> rules = new ArrayList<>();
-
-        Rule rule = Mockito.mock(Rule.class);
-        when(rule.isIsDefault()).thenReturn(Boolean.FALSE);
-        
-        Rule defaultRule = Mockito.mock(Rule.class);
-        when(defaultRule.isIsDefault()).thenReturn(Boolean.TRUE);
-
-        List<Condition> conditions = new ArrayList<>();
-        Condition condition = Mockito.mock(Condition.class);
-
-        when(condition.getAttribute()).thenReturn("gender");
-        when(condition.getOperator()).thenReturn(Operator.EQUALS);
-        when(condition.getType()).thenReturn(AttributeType.STRING);
-
-        List<String> values = new ArrayList<>();
-        values.add("male");
-
-        when(condition.getValues()).thenReturn(values);
-
-        conditions.add(condition);
-        
-        when(rule.getConditions()).thenReturn(conditions);
-
-        Variation variationON = Mockito.mock(Variation.class);
-        Variation variationOFF = Mockito.mock(Variation.class);
-        Variation defaultVariation = Mockito.mock(Variation.class);
-
-        final String allowList = "user123";
-        
-        when(variationON.getAllowList()).thenReturn(allowList);
-
-        when(variationON.getKey()).thenReturn(varKeyON);
-        when(variationON.getRolloutPercentage()).thenReturn(50L);
-        
-        when(variationOFF.getKey()).thenReturn(varKeyOFF);
-        when(variationOFF.getRolloutPercentage()).thenReturn(50L);
-        
-        when(defaultVariation.getKey()).thenReturn(varKeyDefaultRule);
-        when(defaultVariation.getRolloutPercentage()).thenReturn(100L);
-
-        List<Variation> variations = new ArrayList<>();
-        variations.add(variationON);
-        variations.add(variationOFF);
-        
-        when(rule.getVariations()).thenReturn(variations);
-
-        List<Variation> defaultRuleVariations = new ArrayList<>();
-        defaultRuleVariations.add(defaultVariation);
-
-        when(defaultRule.getVariations()).thenReturn(defaultRuleVariations);
-        
-        rules.add(rule);
-        rules.add(defaultRule);
-
-        when(flag.getRules()).thenReturn(rules);
-        when(flag.getDefaultRule()).thenReturn(defaultRule);
-
-        when(flag.getVariations()).thenReturn(variations);
-        when(flag.getOffVariation()).thenReturn(variationOFF);
-
-        percentRolloutUser = Mockito.mock(UnlaunchUser.class);
-        when(rule.matches(percentRolloutUser)).thenReturn(true);
-
-        UnlaunchStringValue stringValue = Mockito.mock(UnlaunchStringValue.class);
-        when(stringValue.get()).thenReturn("male");
-
-        Map<String, UnlaunchValue> map = new HashMap();
-        map.put("gender", stringValue);
-
-        when(percentRolloutUser.getAllAttributes()).thenReturn(map);
+        setUpDefaultFlag(50L, 50L);
     }
 
     @Test
@@ -152,7 +78,7 @@ public class EvaluatorTest {
         UnlaunchUser user = Mockito.mock(UnlaunchUser.class);
         when(user.getId()).thenReturn(userIdDefaultRule);
 
-        UnlaunchFeature unlaunchFeature = UnlaunchFeature.create(flagKey, varKeyDefaultRule, null, "");
+        UnlaunchFeature unlaunchFeature = UnlaunchFeature.create(flagKey, generalVariation, null, "");
 
         Evaluator instance = new Evaluator();
         UnlaunchFeature result = instance.evaluate(flag, user);
@@ -161,13 +87,16 @@ public class EvaluatorTest {
     }
 
     @Test
-    public void testEvaluateWhenPercentageRolloutIsServed() {
+    public void testPercentageRolloutDistribution() {
         logger.debug("EVALUATE_PERCENTAGE_ROLLOUT");
 
         Evaluator instance = new Evaluator();
 
         int countOFF = 0;
-        for (int i = 0; i < 100; i++) {
+        int countON = 0;
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
             String userPR = String.valueOf(i);
             when(percentRolloutUser.getId()).thenReturn(userPR);
 
@@ -176,14 +105,102 @@ public class EvaluatorTest {
             if (result.getVariation().equals(varKeyOFF)) {
                 countOFF++;
             }
-        }
 
-        logger.info("count {}", countOFF);
-        assertTrue(countOFF < 60 && countOFF > 40);
+            if (result.getVariation().equals(varKeyON)) {
+                countON++;
+            }
+        }
+        long totalTime = System.currentTimeMillis() - startTime;
+
+        logger.info("countOFF was {}. Total time {} millis", countOFF, totalTime);
+
+        assertTrue(countOFF < 600 && countOFF > 400);
+        assertTrue(countON < 600 && countON > 400);
     }
 
     @Test
-    public void testEvaluateWhenFlagDisabledOffVariationIsServed() {
+    public void testWhen_RolloutPercentageIsIncreased_Then_PreviouslyAssignedUsersKeepTheirOldVariation() {
+        Map<String, String> userOnVariations = new HashMap<>();
+
+        // Part 1: start with ON and 10 and store assigned variations
+        setUpDefaultFlag(10, 90);
+        Evaluator instance = new Evaluator();
+        int countON = 0;
+        for (int i = 0; i < 100; i++) {
+            when(percentRolloutUser.getId()).thenReturn(String.valueOf(i));
+
+            UnlaunchFeature result = instance.evaluate(flag, percentRolloutUser);
+
+            if (result.getVariation().equals(varKeyON)) {
+                countON++;
+                userOnVariations.put(String.valueOf(i),result.getVariation() );
+            }
+        }
+        assertTrue(countON > 5 && countON < 15);
+        // ---
+
+        // Part 2:  increase percentage of ON; previously assigned users should keep their variation
+        setUpDefaultFlag(40, 60);
+        countON = 0;
+        for (int i = 0; i < 100; i++) {
+            when(percentRolloutUser.getId()).thenReturn(String.valueOf(i));
+
+            UnlaunchFeature result = instance.evaluate(flag, percentRolloutUser);
+
+            if (result.getVariation().equals(varKeyON)) {
+                countON++;
+                userOnVariations.put(String.valueOf(i),result.getVariation() );
+            } else {
+                if (userOnVariations.containsKey(String.valueOf(i))) {
+                    Assert.fail("previously ON assigned user changed after %age of ON increased i.e. rolled out");
+                }
+            }
+        }
+        assertTrue(countON > 35 && countON < 45);
+        // ---
+
+        // Part 3: increase percentage of ON; previously assigned users should keep their variation
+        setUpDefaultFlag(75, 25);
+        countON = 0;
+        for (int i = 0; i < 100; i++) {
+            when(percentRolloutUser.getId()).thenReturn(String.valueOf(i));
+
+            UnlaunchFeature result = instance.evaluate(flag, percentRolloutUser);
+
+            if (result.getVariation().equals(varKeyON)) {
+                countON++;
+                userOnVariations.put(String.valueOf(i),result.getVariation() );
+            } else {
+                if (userOnVariations.containsKey(String.valueOf(i))) {
+                    Assert.fail("previously ON assigned user changed after %age of ON increased i.e. rolled out");
+                }
+            }
+        }
+        assertTrue(countON > 65 && countON < 80);
+        // ---
+    }
+
+    @Test
+    @Ignore //On Umer's Macbook ~ 7-11 seconds
+    public void testWhen_1000VariationsAreEvaluatedAgainstBucketing_Then_ItTakesLessThan15000MilliSeconds() {
+        Evaluator instance = new Evaluator();
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            when(percentRolloutUser.getId()).thenReturn(String.valueOf(i));
+            instance.evaluate(flag, percentRolloutUser);
+        }
+        long totalTime = System.currentTimeMillis() - startTime;
+
+        logger.info("Total time {} millis", totalTime);
+
+        assertTrue(totalTime < 15000);
+    }
+
+
+
+    @Test
+    public void testWhen_FlagIsDisabled_Then_DefaultVariationIsServed() {
         logger.debug("EVALUATE_FLAG_DISABLED_OFF_VARIATION_SERVED");
 
         when(flag.isEnabled()).thenReturn(Boolean.FALSE);
@@ -196,7 +213,86 @@ public class EvaluatorTest {
         UnlaunchFeature result = instance.evaluate(flag, user);
 
         assertEquals(unlaunchFeature.getVariation(), result.getVariation());
+    }
 
+    private void setUpDefaultFlag(long percentageVariationON, long percentageVariationOFF) {
+        evaluator = Mockito.mock(Evaluator.class);
+
+        flag = Mockito.mock(FeatureFlag.class);
+        when(flag.isEnabled()).thenReturn(true);
+        when(flag.getKey()).thenReturn(flagKey);
+
+        List<Rule> rules = new ArrayList<>();
+
+        Rule rule = Mockito.mock(Rule.class);
+        when(rule.isIsDefault()).thenReturn(Boolean.FALSE);
+
+        Rule defaultRule = Mockito.mock(Rule.class);
+        when(defaultRule.isIsDefault()).thenReturn(Boolean.TRUE);
+
+        List<Condition> conditions = new ArrayList<>();
+        Condition condition = Mockito.mock(Condition.class);
+
+        when(condition.getAttribute()).thenReturn("gender");
+        when(condition.getOperator()).thenReturn(Operator.EQUALS);
+        when(condition.getType()).thenReturn(AttributeType.STRING);
+
+        List<String> values = new ArrayList<>();
+        values.add("male");
+
+        when(condition.getValues()).thenReturn(values);
+
+        conditions.add(condition);
+
+        when(rule.getConditions()).thenReturn(conditions);
+
+        Variation variationON = Mockito.mock(Variation.class);
+        Variation variationOFF = Mockito.mock(Variation.class);
+        Variation defaultVariation = Mockito.mock(Variation.class);
+
+        final String allowList = "user123";
+
+        when(variationON.getAllowList()).thenReturn(allowList);
+
+        when(variationON.getKey()).thenReturn(varKeyON);
+        when(variationON.getRolloutPercentage()).thenReturn(percentageVariationON);
+
+        when(variationOFF.getKey()).thenReturn(varKeyOFF);
+        when(variationOFF.getRolloutPercentage()).thenReturn(percentageVariationOFF);
+
+        when(defaultVariation.getKey()).thenReturn(generalVariation);
+        when(defaultVariation.getRolloutPercentage()).thenReturn(100L);
+
+        List<Variation> variations = new ArrayList<>();
+        variations.add(variationON);
+        variations.add(variationOFF);
+
+        when(rule.getVariations()).thenReturn(variations);
+
+        List<Variation> defaultRuleVariations = new ArrayList<>();
+        defaultRuleVariations.add(defaultVariation);
+
+        when(defaultRule.getVariations()).thenReturn(defaultRuleVariations);
+
+        rules.add(rule);
+        rules.add(defaultRule);
+
+        when(flag.getRules()).thenReturn(rules);
+        when(flag.getDefaultRule()).thenReturn(defaultRule);
+
+        when(flag.getVariations()).thenReturn(variations);
+        when(flag.getOffVariation()).thenReturn(variationOFF);
+
+        percentRolloutUser = Mockito.mock(UnlaunchUser.class);
+        when(rule.matches(percentRolloutUser)).thenReturn(true);
+
+        UnlaunchStringValue stringValue = Mockito.mock(UnlaunchStringValue.class);
+        when(stringValue.get()).thenReturn("male");
+
+        Map<String, UnlaunchValue> map = new HashMap();
+        map.put("gender", stringValue);
+
+        when(percentRolloutUser.getAllAttributes()).thenReturn(map);
     }
 
 }
